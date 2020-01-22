3067efdf33935e9439224f310f03129fe4897441
Angela <y1gu@ucsd.edu>
2020-01-21 19:46:22 -0800

    Initial Commit

---
+++ tip calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip calculator
+//
+//  Created by angela on 1/21/20.
+//  Copyright © 2020 angela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

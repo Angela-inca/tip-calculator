d50258829af12c46cfcb4703b0c3ea75dd43e8a6
Angela <y1gu@ucsd.edu>
2020-01-21 22:49:29 -0800

    Fix the default tax percentage

--- tip calculator/ViewController.swift
+++ tip calculator/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+   
+    @IBAction func calculateTip(_ sender: Any) {
+        //GET the bill amount
+        let bill = Double(billField.text!) ?? 0
+        //Calculate the tip and total
+        let tip = bill * Double(tipPercentSlider.value)
+        let total = bill + tip
+        //Update the tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        tipPercentageLabel.text! = String(format:"Tip %: %02d%%",Int(tipPercentSlider.value * 100) )
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 

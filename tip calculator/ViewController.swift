adff984d3b3bd4460d53c352c3e374b5745c862b
Angela <y1gu@ucsd.edu>
2020-01-22 00:07:12 -0800

    fix the

--- tip calculator/ViewController.swift
+++ tip calculator/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var numPeopleSlider: UISlider!
+    @IBOutlet weak var numPeopleLabel: UILabel!
+    @IBOutlet weak var perPersonLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,12 +32,21 @@ class ViewController: UIViewController {
         //GET the bill amount
         let bill = Double(billField.text!) ?? 0
         //Calculate the tip and total
+        let roundedPercent = (round(tipPercentSlider.value * 100)) / 100.0
+        tipPercentSlider.value = roundedPercent
+        let step2: Float = 1
+        let roundedPeople = round(numPeopleSlider.value / step2) * step2
+        numPeopleSlider.value = roundedPeople
         let tip = bill * Double(tipPercentSlider.value)
         let total = bill + tip
+        let perperson = total / Double(numPeopleSlider.value)
+        
         //Update the tip and total labels
         tipLabel.text = String(format: "$%.2f", tip)
         tipPercentageLabel.text! = String(format:"Tip %: %02d%%",Int(tipPercentSlider.value * 100) )
         totalLabel.text = String(format: "$%.2f", total)
+        numPeopleLabel.text = String(format: "Split: %d", Int(numPeopleSlider.value))
+        perPersonLabel.text = String(format: "$%.2f", perperson)
     }
     
 }
